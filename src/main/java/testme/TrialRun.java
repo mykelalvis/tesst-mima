5d41a51fef573a4af2c895d8c76a9d9531f2bb80
Mykel Alvis <mykel.alvis@gmail.com>
2025-04-10 13:57:19 -0500

    Add different sort of constructor

--- src/main/java/testme/TrialRun.java
+++ src/main/java/testme/TrialRun.java
@@ -1,5 +1,27 @@
 package testme;
 
+import static java.util.Objects.requireNonNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.resolution.ArtifactDescriptorException;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.VersionResolutionException;
+
+import eu.maveniverse.maven.mima.context.Context;
+import eu.maveniverse.maven.mima.context.ContextOverrides;
+import eu.maveniverse.maven.mima.context.Runtime;
+import eu.maveniverse.maven.mima.context.Runtimes;
+import eu.maveniverse.maven.mima.extensions.mmr.MavenModelReader;
+import eu.maveniverse.maven.mima.extensions.mmr.ModelRequest;
+import eu.maveniverse.maven.mima.extensions.mmr.ModelResponse;
+
 @javax.inject.Singleton
 @javax.inject.Named
 public class TrialRun {
@@ -8,7 +30,36 @@ public class TrialRun {
   @javax.inject.Inject
   public TrialRun(eu.maveniverse.maven.mima.context.Runtime runtime) {
     this.runtime = runtime;
-    // TODO Auto-generated constructor stub
+  }
+
+  //  @javax.inject.Inject
+//  public TrialRun() {
+//    this.runtime = Runtimes.INSTANCE.getRuntime();
+//
+//  }
+
+  public String model(ContextOverrides overrides, String artifactStr)
+      throws VersionResolutionException, ArtifactResolutionException, ArtifactDescriptorException, IOException {
+    requireNonNull(artifactStr);
+    this.runtime = Runtimes.INSTANCE.getRuntime();
+
+    try (Context context = runtime.create(overrides)) {
+      MavenModelReader mmr = new MavenModelReader(context);
+      ModelResponse response = mmr.readModel(ModelRequest.builder().setArtifact(new DefaultArtifact(artifactStr))
+          .setRequestContext("classpath-demo").build());
+      Model model = response.getEffectiveModel();
+      try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+        String encoding = model.getModelEncoding();
+        if (encoding == null || encoding.length() <= 0) {
+          encoding = "UTF-8";
+        }
+
+        try (Writer out = new OutputStreamWriter(outputStream, encoding)) {
+          new MavenXpp3Writer().write(out, model);
+        }
+        return outputStream.toString(encoding);
+      }
+    }
   }
 
   public static void main(String[] args) {
